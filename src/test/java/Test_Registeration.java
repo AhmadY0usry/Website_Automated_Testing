ba4621ce31d9d46929dccd540ec2b8fbc7496e97
AhmadY0usry <a.yosry344@gmail.com>
2023-06-22 01:09:01 +0300

    editing in compare page (List)

--- src/test/java/Test_Registeration.java
+++ src/test/java/Test_Registeration.java
@@ -1,16 +1,14 @@
-import Pages.Home_Page;
 import Pages.Register_Page;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 
 public class Test_Registeration extends Test_Base {
+    Register_Page registerPage;
     @Test (groups = "register")
     public void testRegister() throws IOException {
         //setUp("edge");
-        Register_Page registerPage;
         registerPage = homePage.clickOnRegister();
         registerPage.fill_Register_Form("Ahmad", "Yosry", "ahmad11@gmail.com", "123456");
         registerPage.clickOnRegisterButton();

b19383e0f148813ebdb952e972eb57f537966e1d
AhmadY0usry <a.yosry344@gmail.com>
2023-06-21 23:17:26 +0300

    Adding test add to cart class

--- src/main/java/Pages/Product_Compare_Page.java
+++ src/main/java/Pages/Product_Compare_Page.java
@@ -14,11 +14,22 @@ public class Product_Compare_Page {
     }
 
     private By Table_Name = By.xpath("//tr[@class='product-name']//td");
+    private By clear_list=By.className("clear-list");
     private List<WebElement> list;
+    private By no_Data=By.className("no-data");
 
     public String checkNameCompare(int index)
     {
         list=driver.findElements(this.Table_Name);
         return list.get(index).getText();
+    }
+
+    public void clear_List()
+    {
+        driver.findElement(this.clear_list).click();
+    }
+    public boolean chkClearListFunctionality(String isClear)
+    {
+        return driver.findElement(this.no_Data).getText().contains(isClear);
     }
 }
--- src/test/java/Test_Compare_Functionality.java
+++ src/test/java/Test_Compare_Functionality.java
@@ -9,7 +9,7 @@ public class Test_Compare_Functionality extends Test_Base {
     Search_Item_Page searchItemPage;
     Product_Compare_Page productComparePage;
     String first_product;
-    String second_product ;
+    String second_product;
 
     @Test
     public void selectFirstProductToCompare() {
@@ -24,18 +24,33 @@ public class Test_Compare_Functionality extends Test_Base {
             homePage.searchForItem("asus");
             searchItemPage = homePage.selectFromSuggestion(0);
             searchItemPage.clickOnAddToCompare();
+
+
     }
-    public Search_Item_Page retunSearchitemPage () {
+    public Search_Item_Page returnSearchItemPage() {
             return searchItemPage;
         }
     @Test (dependsOnMethods = {"selectFirstProductToCompare","selectSecondProductToCompare"})
     public void chkCompareList()
     {
-        searchItemPage=retunSearchitemPage();
+        searchItemPage= returnSearchItemPage();
         productComparePage=searchItemPage.clickOnProductComparison();
         first_product = productComparePage.checkNameCompare(1);
         second_product = productComparePage.checkNameCompare(2);
         assertTrue(first_product.contains("Asus N551JK-XO076H Laptop"));
         assertTrue(second_product.contains("Apple MacBook Pro 13-inch"));
+    }
+    public Product_Compare_Page returnProductComparePage()
+    {
+        return productComparePage;
+    }
+    @Test (dependsOnMethods = "chkCompareList")
+    public void TestRemoveFunctionality()
+    {
+        boolean IsListClear;
+        productComparePage = returnProductComparePage();
+        productComparePage.clear_List();
+        IsListClear=productComparePage.chkClearListFunctionality("You have no items to compare");
+        assertTrue(IsListClear);
     }
 }
